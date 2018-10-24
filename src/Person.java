bb40260c8b66a01fa97dd8049cd37a130a975917
BT_1N3_05@bktech.local <BT_1N3_05@bktech.local>
2018-10-24 14:45:09 -0400

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String firstName, familyName;
+    public Person(String firstName, String familyName){
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFamilyName(){
+        return familyName;
+    }
+    public void setFamilyName (String familyName){
+        this.familyName = familyName;
+    }
+    public boolean equals (Person person){
+        if(this.firstName.equals(person.getFirstName()) && this.familyName.equals(person.getFamilyName())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

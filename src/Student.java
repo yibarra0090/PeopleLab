7a4c69386b48651b84e8d15624e374c89e93bdb5
deakurti <dkurti4965@bths.edu>
2018-10-25 14:48:07 -0400

    updated

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Person {
+    private double GPA;
+    public Student (double GPA)
+    {
+        super("Student", "familyName")
+        this.GPA = GPA;
+    }
+
+
+}

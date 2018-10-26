972fc383bb992b5093e6e7b14c3cae669435aea8
NYC <yibarra0090>
2018-10-25 21:26:42 -0400

    Teacher almost done

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,16 @@
 public class Teacher extends Person {
+    private String Subject;
+    private String Title;
+    public Teacher(String Subject, String Title){
+        super(Teacher);
+        this.Subject = Subject;
+        this.Title = Title;
+    }
+    public String getSubject(){
+        return Subject;
+    }
+    public String toString(){
+        return Title+""+"."+" "+FamilyName;
+    }
+
 }

9121087a900fe29f8f078a21e2fe5eb2c7287caa
NYC <yibarra0090>
2018-10-27 19:13:46 -0400

    almost done

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,25 @@
 public class Student extends Person {
     private double GPA;
-    public Student (double GPA)
-    {
-        super("Student", "familyName")
+    private double height;
+    private int age;
+    public Student(String firstName, String familyName, double height, int age){
+        super(firstName, familyName);
         this.GPA = GPA;
+        this.height = height;
+        this.age = age;
+    }
+    public double getGPA(){
+        return GPA;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String toString(){
+        return this.getFirstName() + ", " + this.getFamilyName();
     }
 
-
 }
+

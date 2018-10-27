95c6c098bc0b1a73f986be402e903223e35aa8b1
NYC <yibarra0090>
2018-10-27 19:19:31 -0400

    almost done

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,8 +1,29 @@
 public class Classroom {
-private Student[] students;
-private Teacher teacher;
+    private Student[] students;
+    private Teacher teacher;
 
-public students(String[], student){
-    this.student = student;
-}
+    public Classroom (Student[] students, Teacher teacher){
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    public String getSubject(){
+        return teacher.getSubject();
+    }
+
+    public double classAverage(){
+        double sum = 0;
+        for (Student s : students){
+            sum += s.getGPA();
+        }
+        return sum/ students.length;
+    }
+
+    public void printClass(){
+        System.out.println(teacher.toString());
+        for (Student s : students){
+            System.out.println(s.toString());
+        }
+    }
+
 }

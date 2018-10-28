a77ebc8384524a27e834ffbdff55ddef47331b44
NYC <yibarra0090>
2018-10-27 20:54:37 -0400

    almost done

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner{
+    private static String [] firstNames = {"Jack","Nick","Yussef","Wei","Dea"};
+    private static String [] familyNames = {"Bob","Colsen","Ibarra","Chen","Kurti"};
+    public static void main(String[] args){
+        Person teacher = new Teacher("Nathan","Levin","Computer Science","Mr");
+
+    }
+}

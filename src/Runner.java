98f71fefe7ceff61d9a60fa20bc04b4ba518c2c5
NYC <yibarra0090>
2018-10-27 22:10:15 -0400

    almost done

--- src/Runner.java
+++ src/Runner.java
@@ -5,11 +5,11 @@ public class Runner{
         Teacher teacher = new Teacher("Nathan","Levin","Computer Science","Mr");
         Student [] student;
         student = new Student[5];
-        student[0] = new Student(firstNames[1], familyNames[1], 3.6, "Male", 16);
-        student[1] = new Student(firstNames[2],familyNames[2], 2.6, "Female", 12);
-        student[2] = new Student(firstNames[3],familyNames[3], 4.0, "Male", 17);
-        student[3] = new Student(firstNames[4],familyNames[4], 4.0, "Male", 17);
-        student[4] = new Student(firstNames[5],familyNames[5], 1.0, "Female", 17);
+        student[0] = new Student(firstNames[0],familyNames[0], 3.6, "Male", 16);
+        student[1] = new Student(firstNames[1],familyNames[1], 2.6, "Female", 12);
+        student[2] = new Student(firstNames[2],familyNames[2], 4.0, "Male", 17);
+        student[3] = new Student(firstNames[3],familyNames[3], 4.0, "Male", 17);
+        student[4] = new Student(firstNames[4],familyNames[4], 1.0, "Female", 17);
         Classroom classroom = new Classroom(student,teacher);
     }
 }
